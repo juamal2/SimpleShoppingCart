84b4b9d2a1a45adb3512097b8937856d4f755eb0
Juamal <juamal27@gmail.com>
2025-11-10 14:39:30 +0000

    Task 2 Offers for Apples and oranges

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -1,9 +1,13 @@
 
 
 class CheckoutSystem {
-  
+
   def Checkout (items: Seq[ShoppingItem]): BigDecimal = {
     items.map(_.value).sum
+  }
+  
+  def CheckoutWithOffers (items: Seq[ShoppingItem]): BigDecimal = {
+    items.distinct.map(_.applyOffer(items)).sum
   }
 
 }
--- src/main/scala/ShoppingItem.scala
+++ src/main/scala/ShoppingItem.scala
@@ -1,5 +1,19 @@
-sealed trait ShoppingItem {val value: BigDecimal; val name: String}
+sealed trait ShoppingItem {
+  val value: BigDecimal
+  val name: String
+  val offerAmount: Int
 
-case object Apple extends ShoppingItem{ val value = 0.60; val name = "apple"}
+  def applyOffer(items: Seq[ShoppingItem]): BigDecimal = {
+    val total = items.count(_.name == name)
+    val totalToDiscount = items.count(_.name == name) / offerAmount
+    (total * value) - (totalToDiscount * value)
+  }
+}
 
-case object Orange extends ShoppingItem{ val value = 0.25; val name = "orange"}
+case object Apple extends ShoppingItem{ val value = 0.60; val name = "apple"
+  override val offerAmount: Int = 2
+}
+
+case object Orange extends ShoppingItem{ val value = 0.25; val name = "orange"
+  override val offerAmount: Int = 3
+}
